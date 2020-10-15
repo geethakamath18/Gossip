475d5c21e05f4d6e09ab9d99b7cd700c6817b4af
SKeerthi02 <57923817+SKeerthi02@users.noreply.github.com>
2020-10-15 19:18:56 -0400

    Adding comments
    comments were added for better understanding

--- project.fsx
+++ project.fsx
@@ -16,31 +16,33 @@ let mutable flag = false
 let mutable n=0 //Number of nodes
 let mutable total = 0
 let mutable time =  System.Diagnostics.Stopwatch() 
-let mutable algorithm = ""
+let mutable algorithm = "" //Input for algorithm
 let mutable waittime = 0
 
-let mutable echoActorsPS=new List<IActorRef>()
+let mutable echoActorsPS=new List<IActorRef>() // Creating a list for actors
 
 //Creating Master Actors
-let mutable masterActor=new List<IActorRef>()
-let mutable masterActorPS=new List<IActorRef>()
+let mutable masterActor=new List<IActorRef>() // Creating a master actor for Gossip
+let mutable masterActorPS=new List<IActorRef>() // Creating a master actor for Push Sum
 
-let mutable m = Map.empty<int, List<int>>
+let mutable m = Map.empty<int, List<int>> // Creating a map for actors and the list of neighbours
 
-let mutable sumMap = Map.empty<int, float>
-let mutable weightMap = Map.empty<int, float>
+let mutable sumMap = Map.empty<int, float> // Creating a map to store the s values for s for each of the actors
+let mutable weightMap = Map.empty<int, float> // Creating a map to store the s values for w for each of the actors
 
 
-let mutable t = ""
-let mutable finish = new List<int>()
+let mutable t = "" // Variable to store the topology string
+let mutable finish = new List<int>() // List to store nodes which have converged
 
 let random = Random()
 let mutable rnd=0
 
-
+// Function to introduce delay
 let wait() = 
     for i = 1 to 100000 do 
         waittime <- waittime+1 
+
+// Function to introduce delay
 let doublewait() = 
     for i = 1 to 1300 do 
         for j = 1 to 100 do 
@@ -56,11 +58,11 @@ type ActorMessageType =
     | Receive of float * float * int
     | Selfsender of float * float * int
 
-let mutable echoActors=new List<IActorRef>()
+let mutable echoActors=new List<IActorRef>() // Creating a list of actors
 
 let mutable heard = new List<int>()
 
-
+// Creating actor system for Gossip protocol
 let gossipAlgo (mailbox:Actor<_>) =
         let mutable count=0
         let rec loop () = actor {
@@ -70,38 +72,36 @@ let gossipAlgo (mailbox:Actor<_>) =
             let mutable delay = 0   
             let mutable actornumber = 0
             match msg with
-            |Self (num, inc) ->
+            |Self (num, inc) -> // Actor receiving the value of Actor ID and the value to increment
                 count <- count + inc
-                // printfn "count:%d node:%d" count num
-                if count >= 0 && count <= 10 then 
+                if count >= 0 && count <= 10 then // Checking count
                     let mutable a = m.[num].Count-1
                     let mutable rand = random.Next(0,a)
-                    // printfn "length of sequenc %d" m.[num].Count
-                    let mutable kee = m.[num].[rand]
-                    mailbox.Self <! Self(num, 0)
+                    let mutable kee = m.[num].[rand] // Choosing random neighbour
+                    mailbox.Self <! Self(num, 0) // Sending message to self
                         
                     if t = "line" then 
                         doublewait()
                     else 
                         wait()
-                    if not (finish.Contains(num)) then    
+                    if not (finish.Contains(num)) then  // Transmitting to a random neighbour  
                         echoActors.[kee] <! Neigbhour(kee, 1)
                     else 
                         let mutable foundNeibhour = false
                         for i = 0 to heard.Count - 1 do 
                             if not (finish.Contains(heard.[i])) then
                                 foundNeibhour <- true
-                                echoActors.[heard.[i]] <! Neigbhour(heard.[i], 1)
+                                echoActors.[heard.[i]] <! Neigbhour(heard.[i], 1) // Checking which nodes are susceptible
                         while finish.Contains(rand) && not foundNeibhour do
                             rand <- random.Next(0,n)
                         if not foundNeibhour then
                             echoActors.[rand] <! Neigbhour(rand, 1)
      
             |Neigbhour (num, inc) ->
-                count <- count+inc
+                count <- count+inc // Incrementing count
                 if count = 5 then
                     masterActor.[0] <! Finished(num)
-                if count = 1 then 
+                if count = 1 then // Checking which nodes are susceptible
                     total <- total+1 
                     heard.Add(num)
 
@@ -117,8 +117,9 @@ let gossipAlgo (mailbox:Actor<_>) =
         }
         loop ()
 
+
+// Actor for Push Sum
 let pushSumAlgo (mailbox:Actor<_>) = 
-    // printfn "Entered actor with random : %d" rnd
     let mutable count=0
     let mutable nodeCount = 0 
     let mutable ratioList=new List<float>()
@@ -128,19 +129,19 @@ let pushSumAlgo (mailbox:Actor<_>) =
         let! msg = mailbox.Receive () //Recieving messages from the mailbox
         
         match msg with
-        | Receive(s_rec, w_rec, nodeNumber) ->
-            let mutable totalSum = 0 |> float
-            let mutable totalWeight = 0|>float
-            totalSum <- sumMap.[nodeNumber]  + s_rec
-            totalWeight <- weightMap.[nodeNumber] + w_rec 
+        | Receive(s_rec, w_rec, nodeNumber) -> // Getting the value of s and w
+            let mutable totalSum = 0 |> float // Total sum
+            let mutable totalWeight = 0|>float // Total weight
+            totalSum <- sumMap.[nodeNumber]  + s_rec // Adding the value of received s to the current value of s
+            totalWeight <- weightMap.[nodeNumber] + w_rec // Adding the value of received s to the current value of w
 
-            totalSum <- totalSum / 2.0
-            totalWeight <- totalWeight / 2.0 
+            totalSum <- totalSum / 2.0 // Halving the sum
+            totalWeight <- totalWeight / 2.0 // Halving the weight
 
-            ratioList.Add(totalSum/totalWeight)
+            ratioList.Add(totalSum/totalWeight) // Adding to a list
 
-            sumMap <- sumMap.Add(nodeNumber, totalSum)
-            weightMap <- weightMap.Add(nodeNumber, totalWeight)
+            sumMap <- sumMap.Add(nodeNumber, totalSum) // Adding sum to the map
+            weightMap <- weightMap.Add(nodeNumber, totalWeight) // Adding weight to the map
 
             let mutable a = m.[nodeNumber].Count-1
             let mutable rand = random.Next(0,a)
@@ -167,7 +168,7 @@ let pushSumAlgo (mailbox:Actor<_>) =
             if ratioList.Count = 1 then 
                 total <- total + 1
 
-
+            // checking conditions to see if the ratio has changed
             if(ratioList.Count>=3 && ((s_rec/w_rec)-ratioList.[ratioList.Count-1])<(10.0**(-10.0)) && ((s_rec/w_rec)-ratioList.[ratioList.Count-2])<(10.0**(-10.0)) && ((s_rec/w_rec)-ratioList.[ratioList.Count-3])<(10.0**(-10.0)) && not (finish.Contains(nodeNumber))) then //Stopping Condition
                 let mutable t = s_rec/ w_rec
 
@@ -184,26 +185,30 @@ let pushSumAlgo (mailbox:Actor<_>) =
     }
     loop ()   
 
+// Spawning actors for Gossip protocol
 let spawnActorsForGossip() = 
     for i in [0 .. n-1] do
         let properties = string(i)
         let actor = spawn system properties gossipAlgo
         echoActors.Add(actor) 
 
+// Spawning actors for Push Sum protocol
 let spawnActorsForPushSum() = 
         for i in [0 .. n-1] do
         let properties = string(i)
         let actor = spawn system properties pushSumAlgo
         echoActorsPS.Add(actor) 
 
+//Assign the value of s and w to each actor
 let assignSumWeight(n) = 
     for i = 0 to n-1 do 
         sumMap <- sumMap.Add(i,float(i))
         weightMap <- weightMap.Add(i, 1.0)
 
+// Creating the line network using maps
 let lineTopology(n:int) = 
 
-    match algorithm with 
+    match algorithm with // Spawning actors based on the algorithm
     | "gossip" ->
         spawnActorsForGossip()
     | "push-sum" -> 
@@ -212,7 +217,7 @@ let lineTopology(n:int) =
     | _ -> 
         printfn "Enter Proper topology Gossip or Push-Sum"
 
-    for i = 0 to n-1 do 
+    for i = 0 to n-1 do  // Creating the network
         let list = new List<int>()
         if i = 0 then 
             list.Add(i+1)
@@ -223,12 +228,12 @@ let lineTopology(n:int) =
             list.Add(i+1)
             list.Add(i-1)
 
-        m <- m.Add(i, list)
+        m <- m.Add(i, list) // Adding the actor number and list of neighbours
 
     let rnd = random.Next(0,n-1)
 
     time <- Stopwatch.StartNew()
-    match algorithm with
+    match algorithm with // Choosing algorithm and transmitting to a random neighbour
     |"gossip" -> 
         echoActors.[rnd] <! Neigbhour(10, 1)
     |"push-sum" ->
@@ -236,10 +241,10 @@ let lineTopology(n:int) =
     |_ ->
         printfn "not a proper algorithm"
 
-
+// Creating the full network using maps
 let fullTopology(n: int)=
 
-    match algorithm with 
+    match algorithm with // Spawning actors based on the algorithm
     |"gossip" ->
         spawnActorsForGossip()
     |"push-sum"-> 
@@ -247,19 +252,19 @@ let fullTopology(n: int)=
     |_ -> 
         printfn "Enter Proper topology Gossip or Push-Sum"
 
-    for i in [0 .. n] do
+    for i in [0 .. n] do // Creating the network
         let l = new List<int>()
         for j in [0 .. n] do
             if j <> i then 
                 l.Add(j)
-        m<-m.Add(i,l)
+        m<-m.Add(i,l) // Adding the actor number and list of neighbours
         // printfn "%A" m
     let rnd = random.Next(0,n-1)
-    assignSumWeight(n)
+    assignSumWeight(n) //Assigning sum weights to each actor
 
     time <- Stopwatch.StartNew()
 
-    match algorithm with
+    match algorithm with // Choosing algorithm and transmitting to a random neighbour
     |"gossip" -> 
         echoActors.[rnd] <! Neigbhour(10, 1)
     |"push-sum" ->
@@ -267,11 +272,11 @@ let fullTopology(n: int)=
     |_ ->
         printfn "not a proper algorithm"
 
-
+// Creating the Imperfect 2D grid using maps
 let twoDgrid(k:int) = 
     let sq = ceil(sqrt(float(k))) |> int
     n <- sq * sq
-    match algorithm with 
+    match algorithm with // Spawning actors based on the algorithm
     | "gossip" ->
         spawnActorsForGossip()
     | "push-sum" -> 
@@ -280,7 +285,7 @@ let twoDgrid(k:int) =
     | _ -> 
         printfn "Enter Proper topology Gossip or Push-Sum"
 
-    for i in [0 .. n-1] do 
+    for i in [0 .. n-1] do  // Creating the network
         let list = new List<int>()
         if i-1 >= 0 && i%sq <> 0 then 
             list.Add(i-1)
@@ -290,12 +295,12 @@ let twoDgrid(k:int) =
             list.Add(i+sq)
         if i-sq >= 0 then 
             list.Add(i-sq)
-        m <- m.Add(i, list)
+        m <- m.Add(i, list) // Adding the actor number and list of neighbours
     
     let rnd = random.Next(0,n-1)
 
     time <- Stopwatch.StartNew()
-    match algorithm with
+    match algorithm with // Choosing algorithm and transmitting to a random neighbour
     |"gossip" -> 
         echoActors.[rnd] <! Neigbhour(10, 1)
     |"push-sum" ->
@@ -303,12 +308,13 @@ let twoDgrid(k:int) =
     |_ ->
         printfn "not a proper algorithm"
 
-
+// Creating the Imperfect 2D grid using maps
 let imptwoDgrid(k:int) = 
-    let sq = ceil(sqrt(float(k))) |> int
-    n <- sq * sq
+    let sq = ceil(sqrt(float(k))) |> int // Rounding up to get a grid
+    n <- sq * sq // Number of nodes in the grid
     let randomlist = new List<int>()
-    match algorithm with 
+    
+    match algorithm with // Spawning actors based on the algorithm
     |"gossip" ->
         spawnActorsForGossip()
     |"push-sum" -> 
@@ -317,8 +323,10 @@ let imptwoDgrid(k:int) =
     |_ -> 
         printfn "Enter Proper Algorithm gossip or push-sum"
 
-    for i in [0 .. n-1] do 
+    for i in [0 .. n-1] do // Creating the network
         let list = new List<int>()
+
+        // Adding all the nodes that surround it as neighbours
         if i-1 >= 0 && i%sq <> 0 then 
             list.Add(i-1)
         if (i+1) % sq <> 0 then 
@@ -328,17 +336,17 @@ let imptwoDgrid(k:int) =
         if i-4 >= 0 then 
             list.Add(i-4)
         let random = System.Random()
-        let mutable number = random.Next(n-1)
+        let mutable number = random.Next(n-1) // Choosing a random neighbour
         while list.Contains(number) || number = i do
             number <- random.Next(0, n-1)
 
         list.Add(number)
-        m <- m.Add(i, list)
+        m <- m.Add(i, list) // Adding the actor number and list of neighbours
     
     let rnd = random.Next(0,n-1)
 
     time <- Stopwatch.StartNew()
-    match algorithm with
+    match algorithm with // Choosing algorithm and transmitting to a random neighbour
     |"gossip" -> 
         echoActors.[rnd] <! Neigbhour(10, 1)
     |"push-sum" ->
@@ -346,24 +354,23 @@ let imptwoDgrid(k:int) =
     |_ ->
         printfn "not a proper algorithm"
 
-
+// Master Actor
 let master (mailbox: Actor<_>) = 
     let rec masterloop() = actor{
         let! msg =  mailbox.Receive ()
-        // printfn "%A" n
         let mutable dummy = 0
         match msg with
-        |Topo (topo, n) ->
+        |Topo (topo, n) -> // Pattern matching for topology
             match topo with
             |"full" -> fullTopology(n)
             |"2D" -> twoDgrid(n)
             |"line" -> lineTopology(n)
             |"imp2D" -> imptwoDgrid(n)
             |_ -> printfn "improper topology select one of these full, 2D, line, imp2D"
-        |Finished (node) ->
+        |Finished (node) -> // Checking which nodes have converged
             if not (finish.Contains(node)) then
                 finish.Add(node)
-            if finish.Count >= n-2 then
+            if finish.Count >= n-2 then // Checking condition
                 flag <- true
         |_ -> printfn "unkown messageType recived"
         return! masterloop ()
@@ -371,12 +378,12 @@ let master (mailbox: Actor<_>) =
     masterloop()
 
 let main (args:string []) =
-    n<-(int) args.[1] //Setting the value of number of nodes
-    algorithm <- args.[3]
-    t<-args.[2]
-    let mast = spawn system "master" master    
-    masterActor.Add(mast)
-    masterActor.[0] <! Topo (t, n)
+    n<-(int) args.[1] // Setting the value of number of nodes
+    algorithm <- args.[3] // Setting the value for the algorithm
+    t<-args.[2] // Setting the value value for the topology
+    let mast = spawn system "master" master  // Spawning master actor  
+    masterActor.Add(mast) // Adding master reference to the master actor system
+    masterActor.[0] <! Topo (t, n) // Sending messages to the master actor about topology
 
     while not flag do 
         wait()
